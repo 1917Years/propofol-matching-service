3feeca8853de6be4bc7946705ee48134cdfb1254
최영찬 <gks54410@gmail.com>
2022-05-27 23:07:32 +0900

    Feat: 알림 메시지 추가

---
+++ src/main/java/propofol/matchingservice/api/feign/MessageFormat.java
@@ -0,0 +1,6 @@
+package propofol.matchingservice.api.feign;
+
+public class MessageFormat {
+    public static final String APPROVE_MESSAGE = "의 팀원 신청이 승인되었습니다.";
+    public static final String REJECT_MESSAGE = "의 팀원 신청이 거절되었습니다.";
+}
