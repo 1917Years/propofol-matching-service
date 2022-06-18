f0af801e4038c254d34d07b6ba8a241a4e70d10b
최영찬 <gks54410@gmail.com>
2022-06-18 15:32:13 +0900

    Feat: 프로젝트 팀 상태, 태그, 생성자 여부 조건 조회 추가

--- src/main/java/propofol/matchingservice/domain/board/repository/BoardRepository.java
+++ src/main/java/propofol/matchingservice/domain/board/repository/BoardRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import propofol.matchingservice.domain.board.entitiy.Board;
 
+import java.util.Collection;
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
@@ -14,7 +15,7 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     @Query("select b from Board b")
     Page<Board> findAll(Pageable pageable);
 
-    @Query("select b from Board b where b.title like %:keyword% and not b.createdBy = :memberId")
+    @Query("select b from Board b where upper(b.title) like upper(concat('%', :keyword, '%')) and not b.createdBy = :memberId")
     Page<Board> findAllByKeyword(Pageable pageable, @Param("keyword") String keyword, @Param("memberId") String memberId);
 
     @Query("select b from Board b where not b.createdBy = :memberId")
@@ -25,4 +26,14 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     @Query("select b from Board b left join fetch b.boardImages bi where b.id = :boardId")
     Optional<Board> findBoardWithImages(@Param("boardId") Long boardId);
+
+    @Query("select bt.board from BoardTag bt join bt.board where bt.tagId in :tagIds and not bt.board.createdBy = :memberId " +
+            "group by bt.board.id order by count(bt) desc, bt.board.id desc")
+    Page<Board> findAllByTagIdsAndNotMine(@Param("tagIds") Collection<Long> tagIds, @Param("memberId") String memberId,
+                                          Pageable pageable);
+
+    @Query("select bt.board from BoardTag bt join bt.board b where bt.tagId in :tagIds and upper(b.title) like upper(concat('%', :keyword, '%'))" +
+            "and not b.createdBy = :memberId group by bt.board.id order by count(bt) desc, bt.board.id desc")
+    Page<Board> findAllByConditions(@Param("tagIds") Collection<Long> tagIds, @Param("keyword") String keyword,
+                                    @Param("memberId") String memberId, Pageable pageable);
 }
